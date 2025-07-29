56d566b95c0eef8f415d7575fd46628025da6279
willtonian1 <williamstom2005@gmail.com>
2025-07-29 20:23:31 +0100

    cv

--- src/assets/Socials.jsx
+++ src/assets/Socials.jsx
@@ -4,7 +4,7 @@ import GitHub from "./github.svg"
 import LinkedIn from "./linkedin.svg"
 import Insta from "./insta.svg"
 import CV from "./cv.png"
-import CVPDF from "./TWWebCV.pdf"
+import CVPDF from "./cv.pdf"
 const Socials = () => {
 
 if(isMobile){
